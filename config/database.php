0ad0ede1233b01647b3e2dcdcfc81b5118dc56eb
Alban <alban.lfbv@gmail.com>
2020-04-24 21:38:37 +0200

    MySQL docker almost ok

--- config/database.php
+++ config/database.php
@@ -1,4 +1,8 @@
 <?php
-    $DB_DSN = "mysql:host=localhost;port=3306;dbname=camagru";
+    $DB_HOST = "db";
+    $DB_PORT = "3306";
+    $DB_NAME = "camagru";
+    $DB_DSN_EXISTS = "mysql:host=" . $DB_HOST . ";port=" . $DB_PORT . ";dbname=" . $DB_NAME;
+    $DB_DSN_CREATE = "mysql:host=" . $DB_HOST . ";port=" . $DB_PORT;
     $DB_USER = "root";
     $DB_PASSWORD = "tutu";
--- config/setup.php
+++ config/setup.php
@@ -2,13 +2,15 @@
 require_once("database.php");
 
 try {
-    echo $_SERVER['DOCUMENT_ROOT'];
-    $db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+    $db = new PDO($DB_DSN_EXISTS, $DB_USER, $DB_PASSWORD);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(\Exception $e) {
-    echo "Impossible d'accéder à la base de données SQLite : ".$e->getMessage();
-    die();
+} catch (\Exception $e) {
+    $db = new PDO($DB_DSN_CREATE, $DB_USER, $DB_PASSWORD);
+    $db->query("CREATE DATABASE IF NOT EXISTS " . $DB_NAME);
+    $db->query("use " . $DB_NAME);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 
 $db->query("CREATE TABLE user (
@@ -19,7 +21,7 @@ $db->query("CREATE TABLE user (
     registrationDate        TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );");
 
-$db->query("CREATE TABLE image (
+$db->query("CREATE TABLE `image` (
     id                      INTEGER PRIMARY KEY,
     userId                  INTEGER,
     pathToImage             TEXT NOT NULL,
@@ -37,7 +39,7 @@ $db->query("CREATE TABLE comment (
     FOREIGN KEY (imageId)   REFERENCES image(id) ON DELETE CASCADE
 );");
 
-$db->query("CREATE TABLE like (
+$db->query("CREATE TABLE `like` (
     userId                  INTEGER NOT NULL,
     imageId                 INTEGER NOT NULL,
     likeDate                TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -48,8 +50,7 @@ $db->query("CREATE TABLE like (
 
 $db->query("CREATE TABLE layer (
     id                      INTEGER PRIMARY KEY,
-    pathToLayer             TEXT NOT NULL,
-    UNIQUE(pathToLayer)
+    pathToLayer             VARCHAR(255) NOT NULL UNIQUE
 );");
 
 ?>
